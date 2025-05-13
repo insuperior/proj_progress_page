463e3301ab1352f7d25160ec99394b7c5b1ad011
Insu Song <songinsu1128@gmail.com>
2025-05-13 14:39:24 +0800

    2nd progress ppt file uploaded

--- src/components/Documentation.js
+++ src/components/Documentation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import proposal from '../assets/Project_Proposal.pdf';
 import progress1 from '../assets/Project_Progress_Update_1.pdf';
+import progress2 from '../assets/Project_Progress_Update_2.pdf';
 
 const Documentation = () => {
     return (
@@ -21,6 +22,10 @@ const Documentation = () => {
                     <tr>
                         <td>Progress Update 1 (April)</td>
                         <td><a href={progress1} download="CCRP_Progress_Update_1.pdf">Download PDF</a></td>
+                    </tr>
+                    <tr>
+                        <td>Progress Update 2 (May)</td>
+                        <td><a href={progress2} download="CCRP_Progress_Update_2.pdf">Download PDF</a></td>
                     </tr>
                 </tbody>
             </table>
